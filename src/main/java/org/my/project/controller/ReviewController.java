318f2ba5a0bf7606c14ba43815560c7216596208
Revan <abusovrvan34@gmail.com>
2026-05-21 15:23:08 +0400

    Review controller added

--- src/main/java/org/my/project/controller/ReviewController.java
+++ src/main/java/org/my/project/controller/ReviewController.java
@@ -1,8 +1,15 @@
 package org.my.project.controller;
 
+import jakarta.validation.Valid;
+import org.my.project.dto.request.ReviewCreateRequest;
+import org.my.project.dto.request.ReviewUpdateRequest;
+import org.my.project.dto.response.ReviewResponse;
 import org.my.project.service.ReviewService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/reviews")
@@ -11,6 +18,32 @@ public class ReviewController {
 
     public ReviewController(ReviewService reviewService) {
         this.reviewService = reviewService;
+    }
+    @GetMapping
+    public ResponseEntity<List<ReviewResponse>> getAllReviews() {
+        return ResponseEntity.ok(reviewService.getAllReviews());
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<ReviewResponse> getReviewById(@PathVariable Long id) {
+        return ResponseEntity.ok(reviewService.getReviewsByID(id));
+    }
+    @PostMapping
+    public ResponseEntity<ReviewResponse> createReview(
+            @Valid @RequestBody ReviewCreateRequest request) {
+        ReviewResponse created = reviewService.createReview(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<ReviewResponse> updateReview(
+            @PathVariable Long id,
+            @Valid @RequestBody ReviewUpdateRequest request) {
+        ReviewResponse updated = reviewService.updateReview(id, request);
+        return ResponseEntity.ok(updated);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteReview(@PathVariable Long id) {
+        reviewService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
 
